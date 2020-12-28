52a7f793f05cbc908a97bd3577412f86839af004
Zoufar <zuf_13@mail.ru>
2020-12-28 13:18:13 +0300

    etoneon Ver +hw3

---
+++ hw3-collections/src/test/java/hw3/collections/AppTest.java
@@ -0,0 +1,10 @@
+package hw3.collections;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void appHasAClass() {
+        App classUnderTest = new App();
+    }
+}
